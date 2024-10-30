e0f802d95ec13f4cc984d0b50e52aa3269819c95
Rony <ronybarua-ethical18>
2024-10-30 11:43:55 +0600

    expense page write off modal is created

--- src/components/layout/customer/components/expenses/ExpenseWriteOffSummary.tsx
+++ src/components/layout/customer/components/expenses/ExpenseWriteOffSummary.tsx
@@ -1,27 +1,35 @@
 // import { Loader2 } from 'lucide-react';
 import { Button } from '@/components/ui/button';
+import { expenseWriteOffs } from '@/utils/dummy';
 import React from 'react';
+import ExpenseWriteOff from './ExpenseWriteOff';
+import { Separator } from '@/components/ui/separator';
 
 function ExpenseWriteOffSummary() {
   return (
     <div>
-      <h1 className="font-bold text-xl text-[#5B52F9] mb-4">Your write-offs</h1>
+      <h1 className="font-bold text-xl text-[#5B52F9] mb-6">Your write-offs</h1>
+
+      {expenseWriteOffs.map((item, index) => (
+        <div key={item.id}>
+          <ExpenseWriteOff
+            id={item.id}
+            title={item.title}
+            value={item.amount}
+            quantity={item.quantity}
+          />
+          {index === 5 && <Separator className="mb-5" />}
+        </div>
+      ))}
 
       <div className="py-3">
         <Button
-          // disabled={loading || status === 'loading'} // Disable button during loading
           type="submit"
           className="w-full bg-[#F0EFFE] text-[#5B52F9] hover:bg-[#F0EFFE] mt-3"
         >
-          {/* {loading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />} */}
           Review deductions and questions
         </Button>
-        <Button
-          // disabled={loading || status === 'loading'} // Disable button during loading
-          type="submit"
-          className="w-full text-white mt-4"
-        >
-          {/* {loading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />} */}
+        <Button type="submit" className="w-full text-white mt-4">
           Import to tax return
         </Button>
       </div>
