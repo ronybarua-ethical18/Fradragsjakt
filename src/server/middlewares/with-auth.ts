a9bcecd97ce07f5c060e6aef0ab13b14ce1fe95f
Rony <ronybarua-ethical18>
2024-11-05 17:41:50 +0600

    custom error handler with api error handler is configured

--- src/server/middlewares/with-auth.ts
+++ src/server/middlewares/with-auth.ts
@@ -1,5 +1,7 @@
-import { TRPCError } from '@trpc/server';
+// /server/middlewares/with-auth.ts
 import { middleware, publicProcedure } from '../trpc';
+import { AuthError } from '@/lib/exceptions';
+import { errorHandler } from './error-handler';
 
 type User = {
   name?: string | null;
@@ -15,16 +17,21 @@ type Context = {
 const withAuth = middleware(async ({ ctx, next }) => {
   const contextWithSession = ctx as Context;
 
-  if (!contextWithSession.user) {
-    throw new TRPCError({ code: 'UNAUTHORIZED' });
-  }
+  try {
+    if (!contextWithSession.user) {
+      throw new AuthError('You must be logged in to access this resource.');
+    }
 
-  return next({
-    ctx: {
-      ...contextWithSession,
-      session: contextWithSession.user,
-    },
-  });
+    return next({
+      ctx: {
+        ...contextWithSession,
+        session: contextWithSession.user,
+      },
+    });
+  } catch (error) {
+    const { message } = errorHandler(error);
+    throw new AuthError(message);
+  }
 });
 
 export const protectedProcedure = publicProcedure.use(withAuth);
