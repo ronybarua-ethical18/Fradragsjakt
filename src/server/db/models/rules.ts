b22c1c9125db69e7b0039b76261b3e938a7040b8
Rony <ronybarua-ethical18>
2024-11-07 13:19:11 +0600

    helper function for expense api is written with some modification in expense model

--- src/server/db/models/rules.ts
+++ src/server/db/models/rules.ts
@@ -27,7 +27,7 @@ const RuleSchema: Schema = new Schema<IRule>(
   { timestamps: true }
 );
 
-RuleSchema.index({ category_title: 1 });
+RuleSchema.index({ category_title: 1, user: 1 });
 RuleSchema.index({ description_contains: 1 });
 
 const RuleModel =
--- src/server/helpers/expense.ts
+++ src/server/helpers/expense.ts
@@ -1,30 +1,59 @@
 import { ExpenseType, IExpense } from '../db/interfaces/expense';
-import { IRule } from '../db/interfaces/rules';
+// import { IRule } from '../db/interfaces/rules';
+import CategoryModel from '../db/models/category';
 import ExpenseModel from '../db/models/expense';
 import RuleModel from '../db/models/rules';
 
-async function findMatchingRule(description: string, userId: string) {
-  return await RuleModel.findOne({
-    description: { $regex: description, $options: 'i' },
-    user: userId,
-  });
-}
+// async function findMatchingRule(description: string, userId: string) {
+//   return await RuleModel.findOne({
+//     description: { $regex: description, $options: 'i' },
+//     user: userId,
+//   });
+// }
 
-async function createExpenseRecord(
-  input: IExpense,
-  rule: IRule,
-  userId: string
-) {
+async function createExpenseRecord(input: IExpense, userId: string) {
+  // Upsert category
+  const category = await CategoryModel.findOneAndUpdate(
+    {
+      title: input.category,
+      creator_id: userId,
+    },
+    {
+      $setOnInsert: {
+        title: input.category,
+        creator_id: userId,
+      },
+    },
+    { upsert: true, new: true }
+  );
+  // Upsert rule
+  const updatedRule = await RuleModel.findOneAndUpdate(
+    {
+      description_contains: input.description,
+      user: userId,
+    },
+    {
+      $setOnInsert: {
+        expense_type: input.expense_type,
+        category_title: input.category,
+        category: category._id,
+      },
+    },
+    { upsert: true, new: true }
+  );
+
+  // Create the expense record with updated or default rule and category information
   return await ExpenseModel.create({
     ...input,
     user: userId,
     expense_type:
-      rule?.expense_type || input.expense_type || ExpenseType.unknown,
-    category: rule?.category || input.category || 'unknown',
+      updatedRule?.expense_type || input.expense_type || ExpenseType.unknown,
+    category: category?.title || input.category || 'unknown',
+    rule: updatedRule?._id,
   });
 }
 
 export const ExpenseHelpers = {
-  findMatchingRule,
+  //   findMatchingRule,
   createExpenseRecord,
 };
